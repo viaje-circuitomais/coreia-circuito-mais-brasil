7fe35ee0a9d3cc448f920c5fef5617d08a7a9f9e
joao.pedro <joaopedrobueno2910@gmail.com>
2023-08-22 20:40:48 -0300

    ajustando modal e header

--- app.js
+++ app.js
@@ -1,3 +1,258 @@
+const PACOTES = [
+  {
+    id: 1,
+    titulo: "Abril 2024",
+    imagem: "assets/templo-modal.webp",
+    body: `
+    <p>
+            Período de 23/04 à 08/05/2024 - Roteiro 1 <br />
+            14 diárias <br />
+           <b><i>O que está incluído:</i></b> <br/>
+            1 - Passagem Aérea GRU/Seoul aeroporto de Incheon/GRU, com franquia
+            de uma bagagem de mão de 10kg e uma mala despachada de 23kg;<br />
+
+            2 - Seguro Viagem com cobertura de COVID;<br />
+
+            3 - Translado em Van privativa do Aeroporto de
+            Incheon/Hotel/Aeroporto de Incheon;<br />
+
+            4 - Autorização de entrada no País (K-ETA);<br />
+
+            5 - Hospedagem em hotel categoria 3 estrelas, quarto duplo ou triplo com cama
+            de solteiro e café da manhã incluído;<br />
+
+            6 - Acompanhamento com Guia Português/Coreano por cinco dias pelo
+            período de 8 horas ao dia;<br />
+
+            7 - KTX Seoul/Busan/Seoul;<br />
+            Observação: Da estação para o hotel e vice-versa, o translado será
+            feito em transporte público ou táxi em grupo de 4 pessoas (pagamento
+            com T-money).<br />
+
+            8 - Cartão T-Money com recarga de 80.000 wons para transporte
+            público ou táxi;<br />
+
+            9- Guia por um dia pelo período de 8 horas em Busan.<br />
+
+            10- Chip de celular com Internet<br /><br />
+
+            <b>ROTEIRO EM SEOUL</b><br />
+            <i>Na Chegada após o Check-in e descanso, às 19:00 horas todos em jantar de confraternização (pagamento à parte)</i><br/>
+            Dia 1 (com o guia):<br />
+            - City tour com ônibus hop on hop off;<br />
+            - Praça Gwanghwamun;<br />
+            - Museu da Praça (Gwanghwamun);<br />
+            - Riacho Cheongyencheon.<br /><br />
+
+            Dia 2 (com o guia):<br />
+            - Palácio Gyeongbokgung; <br />
+            - Museu do Palácio Gyeongbokgung;<br />
+            - Palácio Cheongyencheon;<br />
+            - Traje tradicional Coreano (Hambok) básico (penteados e adereços à parte).<br /><br />
+
+            Dia 3 (com o guia):<br />
+            - Museu Nacional da Coréia;<br />
+            - Visita ao bairro de Myeongdong;<br />
+            - Sauna;<br />
+            - Karaokê (Norebang).<br /><br />
+
+            Dia 4 (com o guia):<br />
+            - Museu da Guerra;<br />
+            - Rio Han com por do sol (sugestão: piquenique).<br /><br />
+
+            Dia 5 (com o guia):<br />
+            - Torre Nansam com Observatório e Teleférico.<br /><br />
+
+            <b>ROTEIRO EM BUSAN</b><br />
+            Dia 1 (com o guia):<br />
+            - City tour em transporte público.<br />
+            Dia 2: Visita à Gamcheon Culture Village.<br />
+            Dia 3: Check-ou no Hotel e retorno para Seoul.<br /><br />
+
+            <b>Incluído:</b><br />
+            1- Dois dias e duas noite; <br />
+            2- Acompanhamento de guia por um dia pelo período de 8 horas;<br />
+            3- Translado estação KTX até o hotel/estação KTX com transporte
+            público ou táxi;<br />
+            4- Hospedagem em hotel categoria 3 estrelas com quarto duplo em
+            cama de solteiro com café da manhã incluso.<br /><br />
+
+            <b><i>Observação: Todos os passeios (Seoul e Busan) serão feitos com
+            transporte público ou táxi em grupo de 4 pessoas pagos com o T-money
+            que será entregue pela Circuito Mais Brasil.</b></i>
+          </p>
+          `,
+  },
+  {
+    id: 2,
+    titulo: "Outubro 2024",
+    imagem: "assets/templo-modal.webp",
+    body: `
+    <p>
+            Período de 23/04 à 08/05/2024 - Roteiro 1 <br />
+            14 diárias <br />
+           <b><i>O que está incluído:</i></b> <br/>
+            1 - Passagem Aérea GRU/Seoul aeroporto de Incheon/GRU, com franquia
+            de uma bagagem de mão de 10kg e uma mala despachada de 23kg;<br />
+
+            2 - Seguro Viagem com cobertura de COVID;<br />
+
+            3 - Translado em Van privativa do Aeroporto de
+            Incheon/Hotel/Aeroporto de Incheon;<br />
+
+            4 - Autorização de entrada no País (K-ETA);<br />
+
+            5 - Hospedagem em hotel categoria 3 estrelas, quarto duplo ou triplo com cama
+            de solteiro e café da manhã incluído;<br />
+
+            6 - Acompanhamento com Guia Português/Coreano por cinco dias pelo
+            período de 8 horas ao dia;<br />
+
+            7 - KTX Seoul/Busan/Seoul;<br />
+            Observação: Da estação para o hotel e vice-versa, o translado será
+            feito em transporte público ou táxi em grupo de 4 pessoas (pagamento
+            com T-money).<br />
+
+            8 - Cartão T-Money com recarga de 80.000 wons para transporte
+            público ou táxi;<br />
+
+            9- Guia por um dia pelo período de 8 horas em Busan.<br />
+
+            10- Chip de celular com Internet<br /><br />
+
+            <b>ROTEIRO EM SEOUL</b><br />
+            <i>Na Chegada após o Check-in e descanso, às 19:00 horas todos em jantar de confraternização (pagamento à parte)</i><br/>
+            Dia 1 (com o guia):<br />
+            - City tour com ônibus hop on hop off;<br />
+            - Praça Gwanghwamun;<br />
+            - Museu da Praça (Gwanghwamun);<br />
+            - Riacho Cheongyencheon.<br /><br />
+
+            Dia 2 (com o guia):<br />
+            - Palácio Gyeongbokgung; <br />
+            - Museu do Palácio Gyeongbokgung;<br />
+            - Palácio Cheongyencheon;<br />
+            - Traje tradicional Coreano (Hambok) básico (penteados e adereços à parte).<br /><br />
+
+            Dia 3 (com o guia):<br />
+            - Museu Nacional da Coréia;<br />
+            - Visita ao bairro de Myeongdong;<br />
+            - Sauna;<br />
+            - Karaokê (Norebang).<br /><br />
+
+            Dia 4 (com o guia):<br />
+            - Museu da Guerra;<br />
+            - Rio Han com por do sol (sugestão: piquenique).<br /><br />
+
+            Dia 5 (com o guia):<br />
+            - Torre Nansam com Observatório e Teleférico.<br /><br />
+
+            <b>ROTEIRO EM BUSAN</b><br />
+            Dia 1 (com o guia):<br />
+            - City tour em transporte público.<br />
+            Dia 2: Visita à Gamcheon Culture Village.<br />
+            Dia 3: Check-ou no Hotel e retorno para Seoul.<br /><br />
+
+            <b>Incluído:</b><br />
+            1- Dois dias e duas noite; <br />
+            2- Acompanhamento de guia por um dia pelo período de 8 horas;<br />
+            3- Translado estação KTX até o hotel/estação KTX com transporte
+            público ou táxi;<br />
+            4- Hospedagem em hotel categoria 3 estrelas com quarto duplo em
+            cama de solteiro com café da manhã incluso.<br /><br />
+
+            <b><i>Observação: Todos os passeios (Seoul e Busan) serão feitos com
+            transporte público ou táxi em grupo de 4 pessoas pagos com o T-money
+            que será entregue pela Circuito Mais Brasil.</b></i>
+          </p>
+          `,
+  },
+  {
+    id: 3,
+    titulo: "Junho 2025",
+    imagem: "assets/templo-modal.webp",
+    body: `
+    <p>
+            Período de 23/04 à 08/05/2024 - Roteiro 1 <br />
+            14 diárias <br />
+           <b><i>O que está incluído:</i></b> <br/>
+            1 - Passagem Aérea GRU/Seoul aeroporto de Incheon/GRU, com franquia
+            de uma bagagem de mão de 10kg e uma mala despachada de 23kg;<br />
+
+            2 - Seguro Viagem com cobertura de COVID;<br />
+
+            3 - Translado em Van privativa do Aeroporto de
+            Incheon/Hotel/Aeroporto de Incheon;<br />
+
+            4 - Autorização de entrada no País (K-ETA);<br />
+
+            5 - Hospedagem em hotel categoria 3 estrelas, quarto duplo ou triplo com cama
+            de solteiro e café da manhã incluído;<br />
+
+            6 - Acompanhamento com Guia Português/Coreano por cinco dias pelo
+            período de 8 horas ao dia;<br />
+
+            7 - KTX Seoul/Busan/Seoul;<br />
+            Observação: Da estação para o hotel e vice-versa, o translado será
+            feito em transporte público ou táxi em grupo de 4 pessoas (pagamento
+            com T-money).<br />
+
+            8 - Cartão T-Money com recarga de 80.000 wons para transporte
+            público ou táxi;<br />
+
+            9- Guia por um dia pelo período de 8 horas em Busan.<br />
+
+            10- Chip de celular com Internet<br /><br />
+
+            <b>ROTEIRO EM SEOUL</b><br />
+            <i>Na Chegada após o Check-in e descanso, às 19:00 horas todos em jantar de confraternização (pagamento à parte)</i><br/>
+            Dia 1 (com o guia):<br />
+            - City tour com ônibus hop on hop off;<br />
+            - Praça Gwanghwamun;<br />
+            - Museu da Praça (Gwanghwamun);<br />
+            - Riacho Cheongyencheon.<br /><br />
+
+            Dia 2 (com o guia):<br />
+            - Palácio Gyeongbokgung; <br />
+            - Museu do Palácio Gyeongbokgung;<br />
+            - Palácio Cheongyencheon;<br />
+            - Traje tradicional Coreano (Hambok) básico (penteados e adereços à parte).<br /><br />
+
+            Dia 3 (com o guia):<br />
+            - Museu Nacional da Coréia;<br />
+            - Visita ao bairro de Myeongdong;<br />
+            - Sauna;<br />
+            - Karaokê (Norebang).<br /><br />
+
+            Dia 4 (com o guia):<br />
+            - Museu da Guerra;<br />
+            - Rio Han com por do sol (sugestão: piquenique).<br /><br />
+
+            Dia 5 (com o guia):<br />
+            - Torre Nansam com Observatório e Teleférico.<br /><br />
+
+            <b>ROTEIRO EM BUSAN</b><br />
+            Dia 1 (com o guia):<br />
+            - City tour em transporte público.<br />
+            Dia 2: Visita à Gamcheon Culture Village.<br />
+            Dia 3: Check-ou no Hotel e retorno para Seoul.<br /><br />
+
+            <b>Incluído:</b><br />
+            1- Dois dias e duas noite; <br />
+            2- Acompanhamento de guia por um dia pelo período de 8 horas;<br />
+            3- Translado estação KTX até o hotel/estação KTX com transporte
+            público ou táxi;<br />
+            4- Hospedagem em hotel categoria 3 estrelas com quarto duplo em
+            cama de solteiro com café da manhã incluso.<br /><br />
+
+            <b><i>Observação: Todos os passeios (Seoul e Busan) serão feitos com
+            transporte público ou táxi em grupo de 4 pessoas pagos com o T-money
+            que será entregue pela Circuito Mais Brasil.</b></i>
+          </p>
+          `,
+  },
+];
+
 const toggleQuestion = (id) => {
   const questionElement = document.querySelector(`#question-${id}`);
   const answerElement = questionElement.querySelector(`.answer-${id}`);
@@ -22,7 +277,23 @@ const modal = document.querySelector(".modal");
 
 const hideModal = () => (modal.style.display = "none");
 const showModal = () => (modal.style.display = "block");
-const openModal = (pacoteId) => showModal();
+const openModal = (pacoteId) => {
+  const pacote = PACOTES.find((pacote) => pacote.id === pacoteId);
+  const modalBody = document.querySelector(".modal-body");
+  const modalTitulo = document.querySelector(".modal-header > h2");
+
+  modalTitulo.textContent = pacote.titulo;
+
+  console.log(modalBody, pacote);
+
+  // corpo do modal
+  modalBody.innerHTML = `
+    <img class="modal-imagem" src="${pacote.imagem}" />
+    <p>${pacote.body}</p>
+  `;
+
+  showModal();
+};
 
 document.querySelector(".modal button").onclick = hideModal;
 document.querySelector(".close-modal-icon").onclick = hideModal;
